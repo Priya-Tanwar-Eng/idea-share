609c6650360272fa927ba6590428bc742164b375
Priya Tanwar <priya8261717@gmail.com>
2025-12-14 22:09:50 +0530

    add .env file in frontend

--- frontend/src/utils/fetchAPI.js
+++ frontend/src/utils/fetchAPI.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/" : "https://idea-share-livid.vercel.app",
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5000/"
+      : import.meta.env.VITE_API_URL,
 });
+
 
 export default API;
