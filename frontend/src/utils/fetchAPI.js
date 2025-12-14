4f83c9f04c0597b1c5afd962905830d1b7b99866
Priya Tanwar <priya8261717@gmail.com>
2025-12-14 21:44:49 +0530

    api change

--- frontend/src/utils/fetchAPI.js
+++ frontend/src/utils/fetchAPI.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/" : "https://idea-share-livid.vercel.app/",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/" : "https://idea-share-livid.vercel.app",
 });
 
 export default API;
