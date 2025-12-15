32bf86c1d7216e7ee60204159c11081e6fee0e6f
Priya Tanwar <priya8261717@gmail.com>
2025-12-15 23:13:51 +0530

    update auth.js

--- backend/api/auth.js
+++ backend/api/auth.js
@@ -1,12 +1,11 @@
 const express = require("express");
+const {registerUser, loginUser} = require("../controllers/authController");
+
+
 const router = express.Router();
 
-router.post("/login", (req, res) => {
-  res.json({
-    success: true,
-    token: "dummy-token",
-    user: { id: 1, name: "Priya" }
-  });
-});
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+
 
 module.exports = router;
